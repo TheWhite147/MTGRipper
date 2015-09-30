42a6f04631b938fec4b51c8e81b7a568573445e2
Marc LeBlanc <ff_thewhite@hotmail.com>
2015-09-30 00:22:21 -0400

    Started to integrate "Show card" button

--- MTGRipperServer/Scripts/script.js
+++ MTGRipperServer/Scripts/script.js
@@ -1,15 +1,15 @@
 // JavaScript source code
 $(document).ready(function () {
 
-    // On set le focus au texte box de username
+    // Set focus on search box
     $("#inputSearch").trigger("focus");    
 
-    //On cache le contenu de la page
+    // Hide page content
     $("#mainContent").hide();
 
-    if (isFormValid()) {
-
-        $("#searchForm").submit(function (event) {
+    // Submit the search form
+    $("#searchForm").submit(function (event) {
+        if (isFormValid()) {
             var spinner = new Spinner(opts).spin();
             $("#spinnerZone").html(spinner.el);
             $("#mainContent").hide();
@@ -23,6 +23,8 @@ $(document).ready(function () {
                 $("#mainContent").show();
 
                 $("#mainContent").html(data);
+                updateControls();
+
             })
             .done(function () {
                 //alert("second success");
@@ -37,16 +39,49 @@ $(document).ready(function () {
             });
 
             event.preventDefault();
-        });
-    }
+        }
+    });
+  
 
-    // Permet de valider le formulaire
+    // Form validation
     function isFormValid() {
-        if ($("#inputSearch").val == "")
+        if ($("#inputSearch").val() == "")
             return false;
 
         return true;
     }
 
+    updateControls();
+
 });
+
+function updateControls() {
+
+    // Show card image button
+    $(".showCardBtn").click(function () {
+        if ($(this).hasClass("imageLoaded")) {
+            if ($(this).hasClass("show")) {
+                $(this).removeClass("show");
+                $(this).hide();
+            } else {
+                $(this).addClass("show");
+                $(this).show();
+            }
+            $(this).show();
+
+        } else {
+
+            var imageSrc = $(this).data("image-src");
+            if (!imageSrc)
+                return;
+
+            $(this).addClass("imageLoaded");
+            $(this).addClass("show");
+            var nextImage = $(this).closest($(".imageContent")).closest("img");
+
+            $(nextImage).attr("src", imageSrc);
+            $(nextImage).show();
+        }
+    });
+}
 
