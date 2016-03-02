5932302a9b63adf853db797fbbcc5087899ee722
Marc LeBlanc <thewhite147@gmail.com>
2016-03-02 16:09:41 -0500

    Three Kings Loot ripping seems to work well

--- MTGRipperServer/Helpers/HtmlParser.cs
+++ MTGRipperServer/Helpers/HtmlParser.cs
@@ -10,7 +10,26 @@ namespace MTGRipperServer.Helpers
         public static string ParsePrice3KL(string html)
         {
             string price = string.Empty;
+            string spanText = "<span class=\"offers";
 
+            html = html.Replace("\n", "").Replace("\t", "").Replace("\r","");
+
+            int indexSpanStart = html.IndexOf(spanText);
+            html = html.Remove(0, indexSpanStart + spanText.Length).Trim();
+
+            string cadText = "CAD$";
+            int indexCAD = html.IndexOf(cadText);
+            html = html.Remove(0, indexCAD + cadText.Length);
+
+            int indexSpanEnd = html.IndexOf("</span>");
+            html = html.Remove(indexSpanEnd).Trim();
+                        
+            price = html.Replace("CAD$", "");
+
+            if (string.IsNullOrEmpty(price))
+                price = "N/A";
+            else
+                price = price + "$";
 
             return price;
         }
