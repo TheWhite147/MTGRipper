557b099deffb41d1fc5c2d16b06732b384aedf4b
Marc LeBlanc <thewhite147@gmail.com>
2016-03-02 15:33:37 -0500

    UI ready to integrate page ripping

---
+++ MTGRipperServer/Helpers/HtmlParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MTGRipperServer.Helpers
+{
+    public static class HtmlParser
+    {
+        public static string ParsePrice3KL(string html)
+        {
+            string price = string.Empty;
+
+
+            return price;
+        }
+    }
+}
